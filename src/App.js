3f0a62ea9f8780da649451d7393e231a021a44d1
nel417 <n1ck@hey.com>
2021-02-11 20:09:34 -0600

    comment cleanup

--- src/App.js
+++ src/App.js
@@ -10,29 +10,25 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  // Main function
   const runCoco = async () => {
     const net = await cocosd.load();
     
-    //  Loop and detect hands
     setInterval(() => {
       detect(net);
     }, 10);
   };
 
   const detect = async (net) => {
-    // Check data is available
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-      // Get Video Properties
+
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
@@ -40,16 +36,11 @@ function App() {
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // 4. TODO - Make Detections
-      // e.g. const obj = await net.detect(video);
       const obj = await net.detect(video)
       // console.log(obj);
 
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
-
-      // 5. TODO - Update drawing utility
-      // drawSomething(obj, ctx)  
       drawRect(obj, ctx);
     }
   };
